9f43a14eb2bc1cc37b3a1ce95406953e222ad094
switch2512 <gietzen.cameron@gmail.com>
2022-02-01 19:54:02 -0800

    Updates

---
+++ precache-manifest.7a5b3ddf301e90be9c7cfe6c7023642b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9f466adb29bdb4190ca37681a9e382",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "3db0de09ce6a5fc000ed",
+    "url": "/reactproject/static/js/2.3f9574a6.chunk.js"
+  },
+  {
+    "revision": "131c826a0c67fb0af362",
+    "url": "/reactproject/static/js/main.25459b51.chunk.js"
+  },
+  {
+    "revision": "70d03459cc4031a25c4b",
+    "url": "/reactproject/static/js/runtime~main.7c4e9af8.js"
+  }
+]);
